0092ccaabe1b7fc9c97a7efef61d89c835bb55ba
Rob Schopke <rob@schopke.com>
2019-05-31 13:29:20 -0600

    Jessie Paxton

--- Client/App/main.js
+++ Client/App/main.js
@@ -1,5 +1,14 @@
 import PostController from "./Components/PostController.js";
 
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
 
 
 
@@ -10,3 +19,4 @@ class App {
     }
   }
 }
+window['app'] = new App()
