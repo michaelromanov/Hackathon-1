2147e98f1840999d5a645c7fc9e8c8d8f258687d
Rob Schopke <rob@schopke.com>
2019-05-31 10:56:11 -0600

    Co-authored-by: clhartman <courtneylarine@gmail.com>

---
+++ Server/db/dbconfig.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+const connectionString = "mongodb+srv://DBuser:DBuser123@cluster0-uwikr.mongodb.net/questgame?retryWrites=true&w=majority"
+
+function _serverError(err) {
+  console.error(err)
+}
+
+mongoose.set("useFindAndModify", false)
+
+let connection = mongoose.connection
+mongoose.connect(connectionString, {
+  useNewUrlParser: true
+})
+
+connection.on('error', _serverError)
+
+connection.once('open', () => {
+  console.log("Connected to DB");
+})
